e8eec839d004125cd84718a867092dc4f8f505cb
Brian Celenza <bcelenza@gmail.com>
2022-10-22 10:16:22 -0700

    Identify TLS 1.3 from supported version extension

--- src/events/tls.rs
+++ src/events/tls.rs
@@ -16,9 +16,9 @@ pub enum TlsVersion {
     TLSv1_3,
 }
 
-impl From<tls_parser::TlsVersion> for TlsVersion {
-    fn from(v: tls_parser::TlsVersion) -> Self {
-        match v {
+impl From<&tls_parser::TlsVersion> for TlsVersion {
+    fn from(v: &tls_parser::TlsVersion) -> Self {
+        match *v {
             tls_parser::TlsVersion::Ssl30 => TlsVersion::SSLv3,
             tls_parser::TlsVersion::Tls10 => TlsVersion::TLSv1_0,
             tls_parser::TlsVersion::Tls11 => TlsVersion::TLSv1_1,
@@ -32,6 +32,53 @@ impl From<tls_parser::TlsVersion> for TlsVersion {
 #[derive(Debug, Serialize)]
 pub enum TlsExtension {
     ServerNameIndication(Vec<String>),
+    SupportedVersions(Vec<TlsVersion>),
+}
+
+#[derive(Default)]
+struct TlsExtensionFlags {
+    pub tls13: bool,
+}
+
+struct TlsExtensionParser;
+impl TlsExtensionParser {
+    pub fn parse<'a>(ext: Option<&[u8]>) -> Result<(TlsExtensionFlags, Vec<TlsExtension>), EventError<'a>> {
+        let mut flags: TlsExtensionFlags = TlsExtensionFlags::default();
+        let mut extensions: Vec<TlsExtension> = Vec::new();
+        if let Some(e) = ext {
+            let parsed = parse_tls_extensions(e);
+            if parsed.is_err() {
+                return Err(EventError::TranslationError("Could not parse TLS extensions"))
+            }
+
+            // Parse all extensions, ignoring those we don't care about.
+            extensions = parsed.unwrap().1.iter().flat_map(|e| {
+                match e {
+                    tls_parser::TlsExtension::SNI(sni) => {
+                        let names: Vec<String> = sni.iter().map(|s| {
+                            let name = String::from_utf8(s.1.to_vec());
+                            name.unwrap()
+                        }).collect();
+                        Some(TlsExtension::ServerNameIndication(names))
+                    },
+                    tls_parser::TlsExtension::SupportedVersions(sv) => {
+                        let versions: Vec<TlsVersion> = sv.iter().map(|v| {
+                            // If 1.3 is specified, we mark this as TLS 1.3 in the output.
+                            // Flag it here so we can avoid iterating back through these later.
+                            if *v == tls_parser::TlsVersion::Tls13 {
+                                flags.tls13 = true;
+                            }
+
+                            TlsVersion::from(v)
+                        }).collect();
+                        Some(TlsExtension::SupportedVersions(versions))
+                    }
+                    _ => None,
+                }
+            }).collect();
+        }
+        Ok((flags, extensions))
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -46,29 +93,12 @@ pub struct ClientHelloEvent {
 impl ClientHelloEvent {
     pub fn from<'a>(packet: &SlicedPacket, t: &TlsClientHelloContents) -> Result<Self, EventError<'a>> {
         let socket_pair = SocketPair::from_packet(packet)?;
-        let mut extensions: Vec<TlsExtension> = Vec::new();
-        if t.ext.is_some() {
-            let ext = parse_tls_extensions(t.ext.unwrap());
-            if ext.is_err() {
-                return Err(EventError::TranslationError("Could not parse TLS extensions"))
-            }
-            extensions = ext.unwrap().1.iter().flat_map(|e| {
-                match e {
-                    tls_parser::TlsExtension::SNI(sni) => {
-                        let names: Vec<String> = sni.iter().map(|s| {
-                            let name = String::from_utf8(s.1.to_vec());
-                            name.unwrap()
-                        }).collect();
-                        Some(TlsExtension::ServerNameIndication(names))
-                    },
-                    _ => None,
-                }
-            }).collect();
-        }
+        let (extension_flags, extensions) = TlsExtensionParser::parse(t.ext)?;
+
         Ok(ClientHelloEvent {
             source: socket_pair.source,
             destination: socket_pair.destination,
-            version: TlsVersion::from(t.version),
+            version: if extension_flags.tls13 { TlsVersion::TLSv1_3 } else { TlsVersion::from(&t.version) },
             ciphers: t.ciphers.iter()
                 .map(cipher_to_string)
                 .collect(),
@@ -83,16 +113,20 @@ pub struct ServerHelloEvent {
     destination: SocketAddress,
     version: TlsVersion,
     cipher: String,
+    extensions: Vec<TlsExtension>,
 }
 
 impl ServerHelloEvent {
     pub fn from<'a>(packet: &SlicedPacket, t: &TlsServerHelloContents) -> Result<Self, EventError<'a>> {
         let socket_pair = SocketPair::from_packet(packet)?;
+        let (extension_flags, extensions) = TlsExtensionParser::parse(t.ext)?;
+
         Ok(ServerHelloEvent {
             source: socket_pair.source,
             destination: socket_pair.destination,
-            version: TlsVersion::from(t.version),
+            version: if extension_flags.tls13 { TlsVersion::TLSv1_3 } else { TlsVersion::from(&t.version) },
             cipher: cipher_to_string(&t.cipher),
+            extensions,
         })
     }
 }
